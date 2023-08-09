99c29c811a2b4641dd7d8c008d4dd9ed9df4a837
Sammara Valente <sammaravs@outlook.com>
2023-08-09 17:47:05 -0300

    primeiro exercício em python: solicitar ao usuário e somar dois números inteiros

---
+++ somaNumeros.py
@@ -0,0 +1,6 @@
+**Desenvolva um algoritmo que solicite ao usuário dois números inteiros. Imprima a soma destes dois números na tela.**
+
+
+n1 = int(input('Escolha um número: '))
+n2 = int(input('Escolha outro número: '))
+print('Você escolheu o número {} e o número {}. A soma entre eles é {}.' .format(n1, n2, n1 + n2))
