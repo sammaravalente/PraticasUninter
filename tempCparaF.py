f8df6ef116557e6173c6fa0c1b868d333a4c76af
Sammara Valente <sammaravs@outlook.com>
2023-08-14 19:42:25 -0300

    Adicionando símbolo de temperatura Fahrenheit.

--- tempCparaF.py
+++ tempCparaF.py
@@ -1,4 +1,4 @@
 celsius = float(input('Qual a temperatura em Celsius? '))
 fahrenheit = (9 * celsius / 5) + 32
 
-print(f'Se a temperatura em Celsius é {celsius}°C, em Fahrenheit é: {fahrenheit}.')
+print(f'Se a temperatura em Celsius é {celsius}°C, em Fahrenheit é: {fahrenheit}°F.')
