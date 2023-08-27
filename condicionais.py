9d09e26be55c9bdfd97e8a45a90d0e042f8b1204
Sammara Valente <sammaravs@outlook.com>
2023-08-27 17:57:29 -0300

    outros comandos pra exercitar

--- condicionais.py
+++ condicionais.py
@@ -21,3 +21,20 @@ oeste = False
 if norte or sul or leste or oeste == True:
     print("Você escapou!")
 
+
+# Condicionais Compostas
+# Se ano é divisível por 4, escreva: "Pode ser um ano bissexto". Caso contrário, escreva: "Definitivamente não é um ano bissexto".
+ano = int(input("Digite o ano: "))
+if ano % 4 == 0:
+    print("Pode ser um ano bissexto.")
+else:
+    print("Definitivamente não é um ano bissexto")
+
+# Se ambas as variáveis booleanas cima e baixo forem True, escreva: "Decida-se!", caso contrário, escreva: "Você escolheu um caminho!"
+cima = False
+baixo = True
+
+if cima and baixo == True:
+    print("Decida-se!")
+else:
+    print("Você escolheu um caminho!")
